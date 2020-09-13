9c4aa8ef1a52eb649c6fd2e9c2624f1d0028c75c
kkahlua <57166073+kkahlua@users.noreply.github.com>
2020-09-13 12:05:50 +0900

    2장 퀴즈

---
+++ quiz.java
@@ -0,0 +1,32 @@
+package hellojava;
+import java.util.Scanner;
+
+public class quiz {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("첫번쨰 원의 중심과 반지름 입력 : ");
+		double a_x = scanner.nextDouble();
+		double a_y = scanner.nextDouble();
+		double a_r = scanner.nextDouble();
+		System.out.print("두번쨰 원의 중심과 반지름 입력 : ");
+		double b_x = scanner.nextDouble();
+		double b_y = scanner.nextDouble();
+		double b_r = scanner.nextDouble();
+		double dif_x,dif_y;
+		if((a_x-b_x)>0)
+			dif_x=a_x-b_x;
+		else
+			dif_x=b_x-a_x;
+		if((a_y-b_y)>0)
+			dif_y=a_y-b_y;
+		else
+			dif_y=b_y-a_y;
+		double l = dif_x*dif_x +dif_y*dif_y;
+		l= Math.sqrt(l);
+		if(l<=a_r+b_r)
+			System.out.print("두 원은 서로 곂친다");
+		else
+			System.out.print("두 원은 서로 곂치지 않는다");
+	}
+}
---
+++ quiz2switch.java
@@ -0,0 +1,37 @@
+package hellojava;
+
+import java.util.Scanner;
+
+public class quiz2switch {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("연산");
+		double a = scanner.nextDouble();
+		char x = scanner.next().charAt(0);
+		double b = scanner.nextDouble();
+		switch(x) {
+			case '+':
+				System.out.print(a+"+"+b +"의 계산 결과는 "+(a+b));
+				break;
+			case '-':
+				System.out.print(a+"-"+b +"의 계산 결과는 "+(a-b));
+				break;
+			case '*':
+				System.out.print(a+"*"+b +"의 계산 결과는 "+(a*b));
+				break;
+			case '/':
+				if(a==0||b==0)
+				{
+					System.out.print("0으로 나눌 수 없습니다.");
+				}
+				else
+				{
+					System.out.print(a+"/"+b +"의 계산 결과는 "+(a/b));
+				}
+				break;
+			default:
+				System.out.print("잘못된 입력을 하셨습니다.");
+		}
+	}
+}
